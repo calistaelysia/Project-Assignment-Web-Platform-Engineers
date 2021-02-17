6d4d9426ea5d1039cdd1463f0ad20d9596b0ede3
Calista Elysia <calista.elysia@binus.edu>
2021-02-17 21:56:37 +0700

    Commit

--- db-app/src/config/db.js
+++ db-app/src/config/db.js
@@ -1,11 +1,23 @@
 const mysql = require('mysql2/promise');
 
-const conn = mysql.createPool({
+let dbConnConfig = {
     host: 'localhost',
     port: '3306',
     user: 'root',
     password: '',
-    database: 'pokemon'
-});
+    database: 'pokemon',
+  }
+  
+  if (process.env.CLEARDB_DATABASE_URL) {
+    const dbConnConfigUrl = new URL(process.env.CLEARDB_DATABASE_URL);
+  
+    dbConnConfig.host = dbConnConfigUrl.host;
+    dbConnConfig.user = dbConnConfigUrl.username;
+    dbConnConfig.password = dbConnConfigUrl.password;
+  
+    delete dbConnConfig.port;
+  }
+  
+  const conn = mysql.createPool(dbConnConfig)
 
 module.exports = conn;
